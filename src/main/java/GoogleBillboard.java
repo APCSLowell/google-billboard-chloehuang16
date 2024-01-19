7138433474fcd573656cd67f02707cb4f37e72d9
chloehuang16 <142624504+chloehuang16@users.noreply.github.com>
2024-01-18 20:19:24 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,27 +1,25 @@
-public class Main {
-    public final static String e= "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
-
-    public static void main(String[] args) {
-        for(int i = 2; i < e.length()-10; i++){
-          String nums = e.substring(i, i+10);
-          double dNum = Double.parseDouble(nums);
-          if(isPrime(dNum)){
-            System.out.println(dNum + "are 10 digits prime");
-            break;
-          }
-          
-        }
-        
+public class GoogleBillboard {
+	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
+	public static void main(final String[] args) {
+		
+		 boolean found=false;
+    for(int i=2;i<e.length()-10;i++){
+     String digits = e.substring(i,i+10);
+double dNum = Double.parseDouble(digits);
+if(isPrime(dNum)==true&&found==false){
+      System.out.println(dNum);
+   found=true;
+      }
     }
-
-    public static boolean isPrime(double dNum) {   
-    if(dNum < 2){  
-        return false;
+	}
+	//Finish this function
+	public static boolean isPrime(double dNum){
+	
+	for(int i=2;i<=Math.sqrt(dNum);i++){
+    if(dNum%i==0){
+      return false;
     }
-    for(int i = 2; i <= Math.sqrt(dNum) + 1; i++) {
-        if(dNum % i == 0){
-            return false;
-        }
-    }
-    return true;  
+	}
+	return true;	
+	}
 }
